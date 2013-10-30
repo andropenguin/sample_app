54a9f3407a7dd3b9e8c17d53af64d9dd5899fd99
andropenguin <andropenguin@gmail.com>
2013-10-30 16:18:17 +0900

    modify Gemfile for IntelliJ Idea

---
+++ out/production/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a220f1b53f856e14bc2fa3e9ef007e3f573fa4f2c2ac9deca7f34030ceaeb2a041330d976cfa73d5c1d9a063f81073f005c6357ebecc79c3b11067233f0bf81c'
