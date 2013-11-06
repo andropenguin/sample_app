802b2a1ca2c85ea75976cda4aef42f82365c1f13
andropenguin <andropenguin@gmail.com>
2013-11-06 11:20:29 +0900

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,3 +7,11 @@
 #  end
 #end
 include ApplicationHelper
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email.upcase
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Capybaraを使用していない場合にもサインインする。
+  cookies[:remember_token] = user.remember_token
+end
